de6fcf73c348ec2af426f87447e33840297e30ed
Akshay Darade <akshaydarade6675@gmail.com>
2025-01-06 14:30:29 +0000

    add like and dislike functionality

--- booker/src/App.jsx
+++ booker/src/App.jsx
@@ -6,17 +6,20 @@ function App() {
     {
       id: crypto.randomUUID(),
       text: "The Great Gatsby",
+      likes: 0,
     },
     {
       id: crypto.randomUUID(),
       text: 1984,
+      likes: 0,
     },
   ]);
 
-  const updateBooks = (book) => {
+  const addBook = (book) => {
     const newBook = {
       id: crypto.randomUUID(),
       text: book,
+      likes: 0,
     };
     setBooks([newBook, ...books]);
   };
@@ -25,18 +28,40 @@ function App() {
     setBooks(books.filter((b) => b.id !== id));
   };
 
+  const likeBook = (id) => {
+    setBooks((prevState) => {
+      return prevState.map((book) =>
+        book.id === id ? { ...book, likes: book.likes + 1 } : book
+      );
+    });
+  };
+
+  const dislikeBook = (id) => {
+    setBooks((prevState) => {
+      return prevState.map((book) =>
+        book.id === id ? { ...book, likes: book.likes - 1 } : book
+      );
+    });
+  };
+
   return (
     <>
       <header>
         <h1>Booker</h1>
       </header>
 
-      <Form onSubmit={updateBooks} />
+      <Form onSubmit={addBook} />
 
       {books.map((book) => (
         <pre key={book.id}>
-          {book.text}{" "}
-          <button onClick={() => deleteBook(book.id)}>Delete</button>
+          <h3>{book.text}</h3>
+          <p>{book.likes}</p>
+
+          <div style={{ display: "flex", gap: "5px" }}>
+            <button onClick={() => likeBook(book.id)}>Like</button>
+            <button onClick={() => dislikeBook(book.id)}>Dislike</button>
+            <button onClick={() => deleteBook(book.id)}>Delete</button>
+          </div>
         </pre>
       ))}
     </>
